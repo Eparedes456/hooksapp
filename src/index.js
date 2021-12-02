e5abe787b10ec7c33f95cf89672f02dbdeb2b94d
Eparedes456 <erickparedes456@gmail.com>
2021-12-01 23:52:31 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { HookApp } from './HookApp';
+import { CounterApp } from './useState/CounterApp';
+
+ReactDOM.render(
+    <CounterApp/>,
+    document.getElementById('root')
+
+)
